41e9c35a2ed33472b6661d75c0af54d2477424f3
郭修远 <guoxiuyuan@guoxiuyuandeMBP.wv.cc.cmu.edu>
2018-07-12 16:57:06 -0400

    rm testStrate.s

--- antlrDemo/testStrategy.s
+++
@@ -1,7 +0,0 @@
-module dos.strategies;
-define boolean isPhishing = testVariable; // testVariable is stored in the Model
-strategy Defense [true] {
-   t0: (isPhishing) -> filterEmail () { // change to done?
-       done
-   }
-}
