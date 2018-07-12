407b5672210cd2898f43cd42fc5026ee01d434e0
senlinzhicheng <senlinzhicheng@gmail.com>
2018-07-12 16:47:41 -0400

    change testStrategy.s

--- antlrDemo/testStrategy.s
+++ antlrDemo/testStrategy.s
@@ -2,6 +2,6 @@ module dos.strategies;
 define boolean isPhishing = testVariable; // testVariable is stored in the Model
 strategy Defense [true] {
    t0: (isPhishing) -> filterEmail () { // change to done?
-       t1: (!isPhishing) -> done;
+       done
    }
 }
