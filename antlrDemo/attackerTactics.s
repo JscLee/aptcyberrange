13f7a3b2d8b03ac397c4a6c72ebe65fe24725ce4
Xiuyuan Guo <gxy6616@163.com>
2018-08-02 15:48:26 -0400

    clean

--- antlrDemo/attackerTactics.s
+++ antlrDemo/attackerTactics.s
@@ -1,17 +1,88 @@
-module attacker.tactics;
-import model "TargetModel" {Blackhat as B, Web as W};
-
+module dos.strategies;
+import model "Model.java" {Model.java as M};
+//import model "ZNewsSys:Acme" { ZNewsSys as M, ZNewsFam as T, ZNewsDosFam as D} ;
+// A is attacker, W is web server
 tactic phishingEmail() {
     condition {
-        exists lb : server in W.servers | !lb.credential;
+        exists lb : server in W.servers | !lb.logFile;
     }
     action {
-        set lbs = {select l : server in W.servers | !l.credential};
+        set lbs = {select l : server in W.servers | !l.logFile};
         for (server l : lbs) {
-            B.sendPhishingEmail(l);
+            A.sendPhishingEmail(l);
         }
     }
     effect {
-        forall lb : server in W.servers | lb.credential;
+        forall lb : server in W.servers | lb.logFile;
     }
 }
+
+// F is FTP server
+tactic crackWebCredential() {
+    condition {
+        exists lb : server in F.servers | lb.logFile;
+        exists lc : server in A.servers | !lc.webCredential;
+    }
+    action {
+        set lbs = {select l : server in F.servers | l.logFile};
+        for (server l : lbs) {
+            A.downloadLogFile(l);
+            A.decodeLogFile(l);
+        }
+    }
+    effect {
+        forall lc : server in A.servers | lc.webCredential;
+    }
+}
+
+// A is attacker, W is web server, P is payment server
+tactic shellInjection() {
+    condition {
+        exists lb : server in A.servers | lb.webCredential; // credential for web server
+        exists lc : server in A.servers | !lc.cardCredential && !lc.cardPassword; // no credential and password for payment server
+    }
+    action {
+        set lbs = {select l : server in A.servers | l.webCredential};
+        for (server l : lbs) {
+            A.login(l.webCredential);
+            A.inject(l.script); // l.script is the php script to be injected
+        }
+    }
+    effect {
+        forall lc : server in A.servers | lc.cardCredential;
+    }
+}
+
+tactic crackCardPassword() {
+    condition {
+        exists lb : server in A.servers | lb.cardCredential && !lb.cardPassword; // credential for payment server
+        
+    }
+    action {
+        set lbs = {select l : server in A.servers | l.cardCredential && !l.cardPassword};
+        for (server l : lbs) {
+            A.crackPasswd(l.cardCredential); // unshadow and crack the password
+            A.storePasswd(l.cardPassword); // store the cracked password
+        }
+    }
+    effect {
+        forall lb : server in A.servers | lb.cardCredential && lb.cardPassword;
+    }
+}
+
+tactic pirateCard() {
+    condition {
+        exists lb : server in A.servers | lb.cardCredential && lb.cardPassword && !lb.gotMoney; // credential for payment server
+    }
+    action {
+        set lbs = {select l : server in A.servers | l.cardCredential && l.cardPassword && !l.gotMoney};
+        for (server l : lbs) {
+            A.firmware(l.cardCredential, l.cardPassword); // execute firmware
+            A.tracsaction(l.cardCredential, l.cardPassword); // execute transactions
+        }
+    }
+    effect {
+        forall lb : server in A.servers | lb.cardCredential && lb.cardPassword && lb.gotMoney;
+    }
+}
+
--- antlrDemo/defenderTactics.s
+++ antlrDemo/defenderTactics.s
@@ -1,31 +1,71 @@
-module defender.tactics;
-import model "Model" {Mail as M};
+module dos.strategies;
+import model "Model.java" {Model.java as M};
 
+// C is mail server (contractor), D is defender, W is web server, P is payment server
 tactic filterEmail() {
     condition {
-        exists lb : M.mail in M.newMail | lb.isPhishingEmail;
+        exists lb : C.mail in C.newMail | lb.isPhishingEmail;
     }
     action {
-        set lbs = {select l : M.mail in M.newMail | l.isPhishingEmail};
-        for (M.mail l : lbs) {
-            M.notify(l, false);
-            M.block(l);
+        set lbs = {select l : C.mail in C.newMail | l.isPhishingEmail};
+        for (C.mail l : lbs) {
+            D.block(l);
         }
     }
     effect {
-        forall lb : M.mail in M.newMail | !lb.isPhishingEmail;
+        forall lb : C.mail in C.newMail | !lb.isPhishingEmail;
     }
 }
- 
-tactic rebootServer() {
+
+//// D is defender
+//tactic rebootServer() {
+//    condition {
+//        // C.time > C.webThreshold;
+//        exists lb : D.ZNewsLBT in C.components | lb.time > C.webThreshold;
+//        // within timewindow or true
+//    }
+//    action {
+//        T.reboot(C);
+//        set C.time = 0;
+//    }
+//    effect {
+//        true;
+//    }
+//}
+
+tactic changeWebPassword() {
     condition {
-        exists lb : D.ZNewsLBT in M.components | lb.time > M.threshold;
+        // W.time > W.threshold;
+        exists lb : W.server in W.components | lb.time > W.threshold;
+        // within timewindow or true
     }
     action {
-        T.reboot(M);
-        set M.time = 0;
+        set lbs = {select l : W.server in W.components | l.time > W.threshold};
+        for (W.server l : lbs) {
+            W.resetPassword("passwd_of_web_apt_18_summer");
+        }
     }
     effect {
         true;
     }
 }
+
+
+tactic changePaymentPassword() {
+    condition {
+        // P.time > P.threshold;
+        exists lb : P.server in P.components | lb.time > P.threshold;
+        // within timewindow or true
+    }
+    action {
+        set lbs = {select l : P.server in W.components | l.time > P.threshold};
+        for (P.server l : lbs) {
+            P.resetPassword("passwd_of_payment_apt_18_summer");
+        }
+    }
+    effect {
+        true;
+    }
+}
+
+
