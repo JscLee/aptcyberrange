45a33db4040f19dd1a3a001a992cac933f949db9
Xiuyuan Guo <gxy6616@163.com>
2018-08-02 18:07:30 -0400

    merge Test

--- antlrDemo/Test.java
+++ antlrDemo/Test.java
@@ -2,7 +2,7 @@ import java.rmi.RemoteException;
 
 public class Test {
 	public static void main(String[] args) {
-		Probe probe = new Probe("128.237.181.50", 15213);
+		Probe probe = new Probe("128.237.200.112", 15213);
 		String path = "/Users/guoxiuyuan/Applications/Dropbox/CMU/project/aptcyberrange/antlrDemo/Probe.java";
 		try {
 			System.out.println(probe.checkFile(path));
