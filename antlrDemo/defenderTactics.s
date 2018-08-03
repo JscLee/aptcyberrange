f186ca60fd39be1a08af2df7b099085b05ece3f1
frankxmc <591743169@qq.com>
2018-08-03 17:32:27 -0400

    Update defenderTactics.s

--- antlrDemo/defenderTactics.s
+++ antlrDemo/defenderTactics.s
@@ -1,6 +1,5 @@
 module dos.strategies;
-
-import model "TargetModel";
+import model "Model.java" {Model.java as M};
 
 // C is mail server (contractor), D is defender, W is web server, P is payment server
 tactic filterEmail() {
@@ -37,14 +36,15 @@ tactic filterEmail() {
 tactic changeWebPassword() {
     condition {
         // W.time > W.threshold;
-        exists lb : W.server in W.components | lb.time > W.threshold;
+        exists lb : W.server in W.components | lb.time > webThreshold;
         // within timewindow or true
     }
     action {
-        set lbs = {select l : W.server in W.components | l.time > W.threshold};
+        set lbs = {select l : W.server in W.components | l.time > webThreshold};
         for (W.server l : lbs) {
             W.resetPassword("passwd_of_web_apt_18_summer");
         }
+        increaseThreshold(webThreshold); // update web server's threshold by adding 20 seconds
     }
     effect {
         true;
@@ -55,14 +55,15 @@ tactic changeWebPassword() {
 tactic changePaymentPassword() {
     condition {
         // P.time > P.threshold;
-        exists lb : P.server in P.components | lb.time > P.threshold;
+        exists lb : P.server in P.components | lb.time > paymentThreshold;
         // within timewindow or true
     }
     action {
-        set lbs = {select l : P.server in W.components | l.time > P.threshold};
+        set lbs = {select l : P.server in W.components | l.time > paymentThreshold};
         for (P.server l : lbs) {
             P.resetPassword("passwd_of_payment_apt_18_summer");
         }
+        increaseThreshold(paymentThreshold); // update payment server's threshold by adding 40 seconds
     }
     effect {
         true;
