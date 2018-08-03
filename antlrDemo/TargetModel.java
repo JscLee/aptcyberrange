5ce649dd56d5ea853c152bd334f12c89d89e88ca
frankxmc <591743169@qq.com>
2018-08-03 17:29:30 -0400

    Update TargetModel.java

--- antlrDemo/TargetModel.java
+++ antlrDemo/TargetModel.java
@@ -10,6 +10,9 @@ public class TargetModel implements Model {
 	Map<String, StitchParser.TacticContext> tactics;
 	HashSet<String> hooks;
 	Probe probe;
+	Map<String, Integer> timeThresholds;
+	// long webThreshold;
+	// long paymentThreshold;
 	// TODO: implement a cache/memory of remote variables, updated by probes on each call
 
 	TargetModel (Map<String,String> serverIP) {
@@ -22,6 +25,12 @@ public class TargetModel implements Model {
 		// probe = new Probe("128.237.200.112", 15213); // testing only
 		// probe = new Probe("18.204.18.19", 1099); // testing only
 		System.out.println("probe created");
+		int webThreshold = System.currentTimeMillis() + 20000;
+		timeThresholds.put("webThreshold", webThreshold);
+		int paymentThreshold = System.currentTimeMillis() + 40000;
+		timeThresholds.put("paymentThreshold", paymentThreshold);
+		System.out.println("webThreshold: " + webThreshold);
+		System.out.println("paymentThreshold: " + paymentThreshold);
 	}
 
 	@Override
@@ -55,12 +64,47 @@ public class TargetModel implements Model {
 	 */
 	@Override
 	public Integer execHook(String id) {
-		if (id.equals("hasCredential")) {
-			System.out.println("execHook: hasCrendential");
+		if (id.equals("hasCard")) {
+			System.out.println("execHook: hasCard"); // has card credential
 			Integer retVal = probe.hasCredential(); // TODO: API from Probe class
 			// model
+		} else if (id.equals("hasLog")) {
+			System.out.println("execHook: hasLogFile"); // has log file
+			Integer retVal = probe.checkFile(); // TODO: API from Probe class
+			// add return
+		} else if (id.equals("validWeb")) {
+			System.out.println("execHook: validWeb"); // has valid web server password
+			Integer retVal = probe.checkWebPasswd(); // TODO: API from Probe class
+			// add return
+		} else if (id.equals("validCard")) {
+			System.out.println("execHook: validCard"); // has valid payment server password
+			Integer retVal = probe.checkPaymentPasswd(); // TODO: API from Probe class
+			// add return
+		} else if (id.equals("suspicious")) {
+			System.out.println("execHook: suspicious"); // has suspicious mail
+			Integer retVal = probe.checkSuspicious(); // TODO: API from Probe class
+			// add return
+		} else if (id.equals("c.time")) {
+			System.out.println("execHook: currentTime"); // show current time
+			return System.currentTimeMillis();
+		} else if (id.equals("WEB_THRESHOLD")) {
+			System.out.println("execHook: webThreshold"); // show web server's threshold
+			return timeThresholds.get("webThreshold");
+		} else if (id.equals("PAYMENT_THRESHOLD")) {
+			System.out.println("execHook: paymentThreshold"); // show payment server's threshold
+			return timeThresholds.get("paymentThreshold");
 		}
 		System.err.println("execHook: name of hook cannot be found");
 		return -1;
+	}
+
+	/*
+	 * Implement all operations right here
+	 */
+	@Override
+	public Integer execOperations(String id) {
+		if (id.equals("increaseThreshold")) {
+			
+		}
 	}
 }
