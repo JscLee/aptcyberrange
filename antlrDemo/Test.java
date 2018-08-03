54b2c17f0249db2459030b395eda98230e9de1f4
Xiuyuan Guo <gxy6616@163.com>
2018-08-03 13:43:16 -0400

    ...

---
+++ antlrDemo/Test.java
@@ -0,0 +1,13 @@
+import java.rmi.RemoteException;
+
+public class Test {
+	public static void main(String[] args) {
+		Probe probe = new Probe("128.237.200.112", 15213);
+		String path = "/Users/guoxiuyuan/Applications/Dropbox/CMU/project/aptcyberrange/antlrDemo/Probe.java";
+		try {
+			System.out.println(probe.checkFile(path));
+		} catch (RemoteException e) {
+			e.printStackTrace();
+		}
+	}
+} 
